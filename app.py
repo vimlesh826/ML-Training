1bf451e6881f2096415e037636c577ec61db00ed
Vimlesh Kumar <vimlesh826@gmail.com>
2022-08-20 21:31:49 +0530

    fix

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask, request, jsonify, json
+from flask import Flask, request, jsonify
 import pickle
 
-app = Flask('ML')
+application = Flask('ML')
 
 
-@app.route('/models/predict-salary', methods=["POST"])
+@application.route('/models/predict-salary', methods=["POST"])
 def predict_salary():
     input_json = request.get_json(force=True)
     predicted_salary = load_model_and_predict_salary(input_json['experience'])
     if predicted_salary is None:
         predicted_salary = 0
-    return common_response(True, 200, predicted_salary)
+    return common_response(True, 200, predicted_salary, "Salary predicted successfully.")
 
-@app.route('/')
+
+@application.route('/')
 def connected():
     return "Working great ;-)"
 
@@ -22,15 +23,14 @@ def load_model_and_predict_salary(experience: int):
     lr_from_pickle = pickle.load(pickle_file)
     prediction_output = lr_from_pickle.predict([[experience]])[0]
     pickle_file.close()
-    return prediction_output
+    return round(prediction_output, 2)
 
 
-def common_response(success: bool = True, response_code: int = 200, data=None):
-    response = jsonify(success=success, status=response_code, data=data)
+def common_response(success: bool = True, response_code: int = 200, data=None, message="Success"):
+    response = jsonify(success=success, status=response_code, data=data, message=message)
     return response
 
 
 if __name__ == '__main__':
-    # from waitress import serve
-    # serve(app, host="0.0.0.0", port=8080)
-    app.run(host="0.0.0.0", port=8080)
+    application.run(host="0.0.0.0", port=8080)
+    # application.run(host="127.0.0.1", port=5000)
